8f175d89b60005e12053023d3ab3679a3d1e9c5a
Timir Gazizov <abzxcvvcxzba@yandex.ru>
2024-01-07 11:55:14 +0300

    Move city check to choose_city

--- resources/actions.py
+++ resources/actions.py
@@ -9,6 +9,4 @@ class Actions:
     def choose_city(self, city):
         browser.element(el.city_choose_xp).click()
         browser.element(f'//b[text()="{city.capitalize()}"]').click()
-
-    def correct_city(self, city):
         browser.element('.information__title').should(have.text(f"{city.capitalize()}"))
